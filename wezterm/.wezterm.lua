706b77bf2a1ad899a3de5e606b6c3251497e37b1
Toru Takahashi <tttol.t4k.b1z@gmail.com>
2025-11-14 06:05:32 +0900

    Update WezTerm configuration settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,10 +3,30 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+------------------------------------
+--- GENERAL
+------------------------------------
+config.initial_cols = 120
+config.initial_rows = 28
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night'
+
+--- Open link by mouse click
+config.mouse_bindings = {
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'SUPER',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
 -- font
 config.font_size =14
 config.font = wezterm.font 'CommitMono Nerd Font'
 
+------------------------------------
+--- TAB
+------------------------------------
 -- tab appearance
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
@@ -50,9 +70,5 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = false
 
-config.initial_cols = 120
-config.initial_rows = 28
--- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Tokyo Night'
 
 return config
