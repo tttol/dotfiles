9cae3f3ac3d4eeb271b614832ebb1ef2a63bb3c1
tttol <tkhstol.929@gmail.com>
2025-12-08 18:35:57 +0900

    noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -17,13 +17,18 @@ return {
         },
         lsp = {
             progress = {
-                enabled = true,
+                enabled = false,
             },
             hover = {
                 enabled = true,
             },
             signature = {
                 enabled = true,
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
         },
     },
