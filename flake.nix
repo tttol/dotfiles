4f7182a959595a69b2c99bb700858f76d5956b39
tttol <tkhstol.929@gmail.com>
2026-01-14 06:11:15 +0900

    add nix file

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "tttol's dotfiles managed by Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."tttol" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [ ./home.nix ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }: {
+  home.username = "tttol";
+  home.homeDirectory = "/Users/tttol";
+  home.stateVersion = "24.05";
+
+  # Neovim
+  # home.file.".config/nvim" = {
+  #   source = ./config/nvim;
+  #   recursive = true;
+  # };
+
+  # WezTerm
+  home.file.".wezterm.lua" = {
+    source = ./config/wezterm/wezterm.lua;
+  };
+
+  # Zsh
+  home.file.".zshrc" = {
+    source = ./config/zsh/.zshrc;
+  };
+
+  # Starship
+  home.file.".config/starship.toml" = {
+    source = ./config/starship/starship.toml;
+  };
+
+  programs.home-manager.enable = true;
+}
